841ba6f457f56ddf796afb6476420c373669f010
Tony Bowden <tony@tmtm.com>
2016-10-12 17:54:32 +0100

    initial scraper
    For now just get a list of all the members in English

---
+++ lib/all_members_page.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require_rel 'page'
+
+class AllMembersPage < Page
+  field :members do
+    noko.css('.dotList3 li').map do |li|
+      id = li.css('a/@onclick').text[/memInfo\('(.*)'\)/, 1]
+      {
+        name: li.text,
+        url:  member_url(id)
+      }
+    end
+  end
+
+  private
+
+  def member_url(id)
+    # defined in `function memInfo`
+    absolute_url("/mem/mem_pro.jsp?mem_code=#{id}")
+  end
+end
