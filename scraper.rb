fc630e4805f01bd163ae6501e98779258a075911
Tony Bowden <tony@tmtm.com>
2016-10-12 18:37:03 +0100

    Move member page to Member::English
    We're about to add a Korean page, so in preparation, move the English
    one to a more specific class

--- scraper.rb
+++ scraper.rb
@@ -17,7 +17,7 @@ data = AllMembersPage.new(LIST_PAGE).to_h
 warn "Found #{data[:members].count} members"
 
 data[:members].each do |mem|
-  member = MemberPage.new(mem[:url]).to_h
-  warn member
-  # ScraperWiki.save_sqlite(%i(id), member)
+  data = MemberPage::English.new(mem[:url]).to_h
+  warn data
+  # ScraperWiki.save_sqlite(%i(id), data)
 end
